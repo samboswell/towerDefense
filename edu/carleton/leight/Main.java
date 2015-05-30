49ac7674317fde934e404f4d0344b55fa1083e82
tuchowj <tuchowj@carleton.edu>
2015-05-30 11:25:06 -0500

    javaFX literally doesn't let you animate anything under any circumstances

--- edu/carleton/leight/Main.java
+++ edu/carleton/leight/Main.java
@@ -1,5 +1,8 @@
 package edu.carleton.leight;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
@@ -8,22 +11,25 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-import java.security.acl.Group;
+
+
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
-        Parent root = (Parent) loader.load();
-
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
+//        Parent root = (Parent) loader.load();
+//
+        AnchorPane root = new AnchorPane();
 
 
 //        Button btn1 = new Button();
@@ -35,11 +41,36 @@ public class Main extends Application {
 //            }
 //        });
 
+        Circle c1 = new Circle(10.0);
 
+        double x = 10.0;
+        double y = 10.0;
+
+
+        c1.setCenterX(x + 1);
+        c1.setCenterY(y + 1);
+
+
+        root.getChildren().add(c1);
+
+
+
+
+
+//        while (x < 100 && y < 100) {
+//            btn1.setLayoutX(x + 1);
+//            btn1.setLayoutY(y + 1);
+//            btn1.setCenterX();
+//
+//        }
         primaryStage.setTitle("Circle Defend'r");
         primaryStage.setScene(new Scene(root, 700, 500));
-//        primaryStage.setResizable(false);
         primaryStage.show();
+
+
+
+//        primaryStage.setResizable(false);
+
     }
 
     public GameScreen createDefaultGameScreen() {
