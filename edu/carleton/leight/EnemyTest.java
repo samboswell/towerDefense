e8831fbfcd5e30eacd0b60e7de5345b4da508310
Bosbetter <boswells@carleton.edu>
2015-06-01 12:25:54 -0500

    made basis for tests, will implement later today

---
+++ edu/carleton/leight/EnemyTest.java
@@ -0,0 +1,44 @@
+package edu.carleton.leight;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by boswells on 6/1/15.
+ */
+public class EnemyTest {
+
+    @org.junit.Test
+    public void testIsFinished() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetHealth() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetSpeed() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetValue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetX() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetY() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSetHealth() throws Exception {
+
+    }
+}
